8d011bbfd3a5ad147ff50e0666ba44e722995aa2
masoudbarati <trbarati@gmail.com>
2013-03-30 00:24:51 -0700

    defining map and mohre class

---
+++ mench.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+import math
+import socket
+
+pygame.init()
+
+class mohre:
+    def __init__(self,color1,pos1):
+        self.color = color
+        self.pos = pos1
+    def moving(self,n):
+        #/////
+#///////////////////////////////////////
+map = []
+red_dest = [0,0,0,0]
+blue_dest = [0,0,0,0]
+green_dest = [0,0,0,0]
+yellow_dest = [0,0,0,0]
+    
